69217e64e5cf226398d574366c180f06e9a7f572
Abel Fernandez <abelfergodevelop@gmail.com>
2018-12-12 22:26:16 +0100

    Clase 14

---
+++ src/main/java/guru/springframework/guruspring5webapp/GuruSpring5webappApplication.java
@@ -0,0 +1,14 @@
+package guru.springframework.guruspring5webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuruSpring5webappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuruSpring5webappApplication.class, args);
+	}
+
+}
+
